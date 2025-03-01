d9033eceadd4dcd8c3709baf4669907d0becb997
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2025-02-28 23:52:50 -0300

    2 Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, meu-primeiro-projeto-latest');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Olá, meu-primeiro-projeto-latest');
   });
 });
